9156279bd5d4c4e7524b26d5e984425661c13fa0
15915940450 <15915940450@139.com>
2018-07-26 10:14:13 +0800

    static, ./public

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const app=express();
 
+app.use(express.static('./public'));
 app.get('/',(req,res)=>res.send('Hello World!'));
 
 app.listen(1594,()=>console.log('aneme app running at localhost:1594'));
