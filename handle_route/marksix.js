cd2c758d739530649c51c083ba0f5a99bd923a4f
15915940450 <15915940450@139.com>
2018-07-26 11:34:01 +0800

    mongo client

--- handle_route/marksix.js
+++ handle_route/marksix.js
@@ -1,4 +1,16 @@
 //mongodb driver and return data
+var MongoClient=require('mongodb').MongoClient;
+var urlMongoDB='mongodb://localhost:27017/ehd';
+MongoClient.connect(urlMongoDB,function(err,client){
+  if(err){
+    throw err;
+  }
+  var db=client.db('ehd');
+  db.collection('testCollection').insertOne({
+    "date":"2018-07-26",
+    "num":Math.ceil(Math.random()*49)
+  });
+})
 module.exports=function(){
   return ('mongodb');
 };
